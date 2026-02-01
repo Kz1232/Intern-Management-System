e8820e33974dc5f214888ab0ae1baa64c0ad56da
Kz1232 <98634693+Kz1232@users.noreply.github.com>
2026-02-01 22:54:36 +0545

    feat(user):Added UserProfile model and intern app to setting

--- intern/models.py
+++ intern/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model):
+    ROLES =(
+        ("INTERN",'Intern'),
+        ("SUPERVISOR","Supervisor")
+    )
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    role = models.CharField(max_length=20,choices=ROLES,null=False,blank=False)
+
+    def __str__(self):
+        return self.user.username
+
