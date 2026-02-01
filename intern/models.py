88b9c96454d777cc1bc06e60f3cfb71f6123364a
Kz1232 <98634693+Kz1232@users.noreply.github.com>
2026-02-01 23:30:01 +0545

    feat(intern):added task model and registered to admin site

--- intern/models.py
+++ intern/models.py
@@ -12,4 +12,20 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+class Task(models.Model):
+    STATUS_CHOICE=(
+        ('COMP','Completed'),
+        ('PEND','Pending'),
+    )
+
+    title = models.CharField(max_length=255)
+    assigned_to =models.ForeignKey(User,on_delete=models.CASCADE)
+    status=models.CharField(choices=STATUS_CHOICE,max_length=20,default="PEND")
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at =models.DateTimeField(null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+    
 
