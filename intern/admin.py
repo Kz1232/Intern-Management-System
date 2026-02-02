f6f02f47f59f2210673b9d85169221dd87009e32
Kz1232 <98634693+Kz1232@users.noreply.github.com>
2026-02-02 15:40:58 +0545

    chore(admin):customized the list_display

--- intern/admin.py
+++ intern/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import UserProfile,Task,Attendence
 # Register your models here.
 
-admin.site.register(UserProfile)
-admin.site.register(Task)
-admin.site.register(Attendence)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display=['id','user','role']
+
+admin.site.register(UserProfile,UserProfileAdmin)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display=['title','assigned_to','status']
+
+admin.site.register(Task,TaskAdmin)
+
+class AttendenceAdmin(admin.ModelAdmin):
+    list_display=['user','date']
+admin.site.register(Attendence,AttendenceAdmin)
