0bc0a357e854a4c1d0fbb4ec330e35822352fb2d
Kz1232 <98634693+Kz1232@users.noreply.github.com>
2026-02-02 15:51:38 +0545

    feat(jwt):Implemeted token_obtain_api

--- Project/urls.py
+++ Project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from rest_framework_simplejwt.views import ( TokenObtainPairView,)
 
 urlpatterns = [
+    path('api/token/',TokenObtainPairView.as_view(),name="token_obtain_pair"),
+
+
     path('admin/', admin.site.urls),
     path('intern/',include('intern.urls')),
 ]
