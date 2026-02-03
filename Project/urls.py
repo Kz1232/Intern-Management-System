e66d0e30beeea54e72ce58ef39a98739141b0284
Kz1232 <98634693+Kz1232@users.noreply.github.com>
2026-02-04 00:03:17 +0545

    chore(Project_urls):Routed refresh token and updated intern to app

--- Project/urls.py
+++ Project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from rest_framework_simplejwt.views import ( TokenObtainPairView,)
+from rest_framework_simplejwt.views import ( TokenRefreshView,)
 from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -42,9 +42,9 @@ urlpatterns = [
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 
-    path('api/token/',TokenObtainPairView.as_view(),name="token_obtain_pair"),
+   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
     path('admin/', admin.site.urls),
-    path('intern/',include('intern.urls')),
+    path('app/',include('intern.urls')),
 ]
