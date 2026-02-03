fe9db043b2d2bdd1167fe6390ad403744b68199c
Kz1232 <98634693+Kz1232@users.noreply.github.com>
2026-02-03 23:53:54 +0545

    chore(intern):Added task and userprofileserializer

--- intern/serializers.py
+++ intern/serializers.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
-from .models import UserProfile
-class UserProfileRegisterSerializer(serializers.ModelSerializer):
+from .models import UserProfile ,Task
+from django.contrib.auth.models import User
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = UserProfile
         fields = ['user','role']
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    assigned_to = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+    class Meta:
+        model = Task 
+        fields=['title','assigned_to','status','created_at','completed_at']
