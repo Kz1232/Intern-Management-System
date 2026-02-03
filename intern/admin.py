4f46c1b44380636006f6543c065242847e2c3a9f
Kz1232 <98634693+Kz1232@users.noreply.github.com>
2026-02-03 18:37:50 +0545

    chore(admin):Modified list display

--- intern/admin.py
+++ intern/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoAdmin
+from django.contrib.auth.models import User
 from .models import UserProfile,Task,Attendence
 # Register your models here.
 
+class UserAdmin(DjangoAdmin):
+    list_display=('id','username')
+    list_display_links = ('username',)
+    search_fields = ('username','email','first_name','last_name')
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
+
+
 class UserProfileAdmin(admin.ModelAdmin):
     list_display=['id','user','role']
+    list_display_links=['user']
 
 admin.site.register(UserProfile,UserProfileAdmin)
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display=['title','assigned_to','status']
+    list_display=['id','title','assigned_to','status']
 
 admin.site.register(Task,TaskAdmin)
 
